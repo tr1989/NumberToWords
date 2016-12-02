24c71a94e34fbed928646631898e68162543dc56
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-02 17:02:26 +0530

    GREEN - Implemented convert method for first testcase

---
+++ src/main/NumberIntoWordsConverter/NumberIntoWordsConverter.java
@@ -0,0 +1,11 @@
+package NumberIntoWordsConverter;
+
+/**
+ * Created by rtulya on 12/2/2016.
+ */
+public class NumberIntoWordsConverter {
+
+    public String convert(int number){
+        return "one";
+    }
+}
