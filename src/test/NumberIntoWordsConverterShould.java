96420f5c7b7f77ebb39a3a8302a453d8aa5de4f4
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-02 17:00:20 +0530

    RED - Added first testcase as convert_1_into_words_as_one

--- src/test/NumberIntoWordsConverterShould.java
+++ src/test/NumberIntoWordsConverterShould.java
@@ -1,4 +1,6 @@
+import NumberIntoWordsConverter.NumberIntoWordsConverter;
 import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * Created by rtulya on 12/2/2016.
@@ -7,7 +9,7 @@ public class NumberIntoWordsConverterShould {
 
     @Test
     public void convert_1_into_words_as_one() {
-        NumberIntoWordsConverter n2wConverter = NumberIntoWordsConverter();
-        assertEquals("one", n2wConverter.convert(1));
+        NumberIntoWordsConverter n2wConverter = new NumberIntoWordsConverter();
+        Assert.assertEquals("one", n2wConverter.convert(1));
     }
 }
