ab33c0560bf75461e3e238327b06e215d77772cf
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-02 17:25:46 +0530

    RED - Added testcase : convert_2_into_words_as_two

--- src/test/NumberIntoWordsConverterShould.java
+++ src/test/NumberIntoWordsConverterShould.java
@@ -12,4 +12,10 @@ public class NumberIntoWordsConverterShould {
         NumberIntoWordsConverter n2wConverter = new NumberIntoWordsConverter();
         Assert.assertEquals("one", n2wConverter.convert(1));
     }
+
+    @Test
+    public void convert_2_into_words_as_two() {
+        NumberIntoWordsConverter n2wConverter = new NumberIntoWordsConverter();
+        Assert.assertEquals("two", n2wConverter.convert(2));
+    }
 }
